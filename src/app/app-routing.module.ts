5340e3c94b21c458a6dea46212105d9dd23f5136
IT4-2019-Sasa-Ilic <ilic.it4.2019@uns.ac.rs>
2022-05-09 15:39:00 +0200

    Frontend,vezbe 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/core/about/about.component';
+import { AutorComponent } from './components/core/autor/autor.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { KlijentComponent } from './components/klijent/klijent.component';
+import { RacunComponent } from './components/racun/racun.component';
+import { TipRacunaComponent } from './components/tip-racuna/tip-racuna.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'klijent', component: KlijentComponent },
+ { path: 'racun', component: RacunComponent },
+ { path: 'tipRacuna', component: TipRacunaComponent },
+ { path: 'home', component: HomeComponent},
+ { path: 'about', component: AboutComponent},
+ { path: 'autor', component: AutorComponent},
+ { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './components/core/home/home.component';
+import { AutorComponent } from './components/core/autor/autor.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { KlijentComponent } from './components/klijent/klijent.component';
+import { RacunComponent } from './components/racun/racun.component';
+import { TipRacunaComponent } from './components/tip-racuna/tip-racuna.component';
+import { KreditComponent } from './components/kredit/kredit.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AutorComponent,
+    AboutComponent,
+    KlijentComponent,
+    RacunComponent,
+    TipRacunaComponent,
+    KreditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
